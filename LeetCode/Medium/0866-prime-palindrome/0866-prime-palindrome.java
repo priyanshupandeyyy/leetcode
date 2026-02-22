da3c8f30032cf36a9d5faea71ac0cc8787f317e0
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-02-23 00:36:44 +0530

    Time: 403 ms (20.5%), Space: 46 MB (43.19%) - LeetHub

---
+++ LeetCode/Medium/0866-prime-palindrome/0866-prime-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    private boolean isprime(int n){
+        if(n<2)return false;
+        int i=2;
+        while(i*i<=n){
+            if(n%i==0)return false;
+            i++;
+        }
+        return true;
+    }
+
+    private boolean ispalindrome(int n){
+        String s=String.valueOf(n);
+        int i=0,j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+    public int primePalindrome(int n) {
+        while(true){
+            if(isprime(n)&&ispalindrome(n)){
+                break;
+            }
+            else n++;
+
+            if(10000000 <= n && n <= 100000000)
+                n = 100000000;
+        }
+        return n;
+    }
+}
