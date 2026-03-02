e53ece2bc53268a498dd497739f2aba28e4f7d76
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-03-02 23:29:04 +0530

    Time: 1 ms (88.31%), Space: 46.9 MB (97.06%) - LeetHub

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,56 @@
 class Solution {
-    private void dfs(int node,List<List<Integer>>adj,boolean[] vis){
-        vis[node]=true;
-        for(int it:adj.get(node)){
-            if(!vis[it]){
-                vis[it]=true;
-                dfs(it,adj,vis);
-            }
-        }
-    }
+    // private void dfs(int node,List<List<Integer>>adj,boolean[] vis){
+    //     vis[node]=true;
+    //     for(int it:adj.get(node)){
+    //         if(!vis[it]){
+    //             vis[it]=true;
+    //             dfs(it,adj,vis);
+    //         }
+    //     }
+    // }
     public int findCircleNum(int[][] isConnected) {
-        int v=isConnected.length;
-        List<List<Integer>>adj=new ArrayList<>(); //changing to adjacency list
-        for(int i=0;i<v;i++){
-            adj.add(new ArrayList<Integer>());
-        } 
-        for(int i=0;i<v;i++){
-            for(int j=0;j<v;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-            boolean[] vis=new boolean[v];
-            int c=0;
-            for(int i=0;i<v;i++){
-                if(!vis[i]){
-                    c++;
-                    dfs(i,adj,vis);
-                }
-            }
+        // int v=isConnected.length;
+        // List<List<Integer>>adj=new ArrayList<>(); //changing to adjacency list
+        // for(int i=0;i<v;i++){
+        //     adj.add(new ArrayList<Integer>());
+        // } 
+        // for(int i=0;i<v;i++){
+        //     for(int j=0;j<v;j++){
+        //         if(isConnected[i][j]==1 && i!=j){
+        //             adj.get(i).add(j);
+        //             adj.get(j).add(i);
+        //         }
+        //     }
+        // }
+        //     boolean[] vis=new boolean[v];
+        //     int c=0;
+        //     for(int i=0;i<v;i++){
+        //         if(!vis[i]){
+        //             c++;
+        //             dfs(i,adj,vis);
+        //         }
+        //     }
     
-        return c;
+        // return c;
+
+
+         //more easy approach
+         int v=isConnected.length;
+         boolean[] vis=new boolean[v];
+         int c=0;
+
+         for(int i=0;i<v;i++){
+            if(!vis[i]){
+                c++;
+                dfs(i,vis,isConnected);
+            }
+         }
+         return c;
+    }
+    private void dfs(int node,boolean[] vis,int[][] isConnected){
+        vis[node]=true;
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[node][j]==1 && !vis[j]) dfs(j,vis,isConnected);
+        }
     }
 }
