9c6d8705f72ede085c566e7b09e3cfcd3b71d795
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-04-27 16:44:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,13 +1,13 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int ans=0;
+        int c=0,sum=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
         for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==k)ans++;
-            }
+            sum+=nums[i];
+            if(map.containsKey(sum-k))c+=map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        return ans;
+        return c;
     }
 }
