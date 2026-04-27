268f6555356ad67a68452e376c04e69facd73c73
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-04-27 17:09:04 +0530

    Time: 23 ms (87.34%), Space: 65.5 MB (60.44%) - LeetHub

--- LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.java
+++ LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.java
@@ -1,22 +1,14 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int n=nums.length;
-        int[] pre=new int[n];
-        pre[0]=(nums[0]== 0) ?-1:1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==0){
-                pre[i]=pre[i-1]-1;
-            }
-            else pre[i]=pre[i-1]+1;
-        }
         HashMap<Integer,Integer>map=new HashMap<>();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(pre[i] == 0)ans=i+1;
-            if(map.containsKey(pre[i])){
-                ans=Math.max(ans,i-map.get(pre[i]));
-            }
-            else map.put(pre[i], i);
+        int sum=0,ans=0;
+        map.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)sum-=1;
+            else sum+=1;
+
+            if(map.containsKey(sum))ans=Math.max(ans,i-map.get(sum));
+            else map.put(sum,i);
         }
         return ans;
     }
