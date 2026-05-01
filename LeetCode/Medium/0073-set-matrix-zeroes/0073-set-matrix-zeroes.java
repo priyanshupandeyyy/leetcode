03e48cfea94615e6992e097dd101c78297dcadac
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-05-01 16:53:54 +0530

    Time: 1 ms (95%), Space: 47.5 MB (58.52%) - LeetHub

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void setZeroes(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int[] row=new int[n];
+        int[] col=new int[m];
+        Arrays.fill(row,0);
+        Arrays.fill(col,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==1 || col[j]==1)mat[i][j]=0;
+            }
+        }
+    }
+}
