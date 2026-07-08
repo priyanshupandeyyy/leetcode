8e5401eb3742db81ed239dba2d2895f0657741a9
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-07-08 23:33:03 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.86%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
-    }
+        // int[] dp=new int[3];
+        // Arrays.fill(dp,-1);
+        // return solve(n,dp);
 
-    private int solve(int n,int[] dp){
+        //Space Optimization
         if(n<=1)return 1;
+        if(n==2)return 2;
 
-        if(dp[n]!=-1)return dp[n];
-        dp[n]=solve(n-1,dp)+solve(n-2,dp);
-        return dp[n];
+        int[] dp=new int[3];
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++){
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=dp[1]+dp[0];
+        }
+        return dp[2];
     }
+
+    // private int solve(int n,int[] dp){
+    //     if(n<=1)return 1;
+
+    //     if(dp[n]!=-1)return dp[n];
+    //     dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    //     return dp[n];
+    // }
 }
