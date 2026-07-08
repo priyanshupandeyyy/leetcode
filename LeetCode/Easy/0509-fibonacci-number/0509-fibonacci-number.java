f2bf4184afd0384bfec0d90851313e1fbd0ebdd5
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-07-08 22:56:38 +0530

    Time: 17 ms (9.12%), Space: 42.2 MB (15.85%) - LeetHub

---
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+        
+    }
+    
+    private int solve(int n,int[] dp){
+        if(n<=1)return n;
+        if(dp[n]!=-1)return dp[n];
+        return solve(n-1,dp)+solve(n-2,dp);
+    }
+}
