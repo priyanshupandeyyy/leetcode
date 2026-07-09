0ce82ee71f1a9c01a0c7a1c239478ba87e12ef4e
Priyanshu Kumar Pandey <143433172+priyanshupandeyyy@users.noreply.github.com>
2026-07-09 19:19:00 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.54%) - LeetHub

---
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(nums,n-1,dp);
+    }
+
+    private int solve(int[] nums,int n, int[] dp){
+        if(n<0) return 0;
+        if(dp[n]!=-1)return dp[n];
+
+        dp[n]=Math.max(solve(nums,n-1,dp),nums[n]+solve(nums,n-2,dp));
+        return dp[n];
+    }
+
+}
